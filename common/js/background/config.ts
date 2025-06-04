c260d0610f7aae62f630722696805f715f36628f
mohit-uniyal-dev <mohituniyal@frootystudios.com>
2025-06-04 17:01:26 +0530

    consistent pattern for indeed based on base url and vjk

--- common/js/background/config.ts
+++ common/js/background/config.ts
@@ -43,7 +43,7 @@ export const config = [
     {
         jobPortal: "Indeed",
         validUrlPatterns: [
-            "^https:\\/\\/[a-z]+\\.indeed\\.com\\/jobs\\/?\\?.*q=[^&\\s]+",
+            "^https:\\/\\/[a-z]+\\.indeed\\.com\\/.*\\?[^\\s]*vjk=[^&\\s]+",
             "^https?:\\/\\/([a-z]{2}\\.)?indeed\\.com\\/.*[?&]vjk=[a-zA-Z0-9]+.*[?&]advn=\\d+.*",
             "https:\\/\\/[^\\/]+\\.indeed\\.com\\/viewjob\\?jk(?:=[^&\\s]+)?",
         ],
