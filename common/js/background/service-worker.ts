a9a31694d4fc32074445a1203d2f3b9c07713364
mohit-uniyal-dev <mohituniyal@frootystudios.com>
2025-06-12 14:42:05 +0530

    feedback changes

--- common/js/background/service-worker.ts
+++ common/js/background/service-worker.ts
@@ -5,105 +5,128 @@ import { config } from "./config";
 import { autoReloadTabs } from "./autoreload";
 
 chrome.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason === "install" || details.reason === "update") {
-    autoReloadTabs();
-  }
+    if (details.reason === "install" || details.reason === "update") {
+        autoReloadTabs();
+    }
 });
 
 // Set up a listener
 MessageBridge.onMessage(async (request, sender) => {
-  const tabId = sender.tab?.id;
-  const currentUrl = sender.tab?.url || "";
+    const tabId = sender.tab?.id;
+    const currentUrl = sender.tab?.url || "";
 
-  console.log("request", request);
-  console.log("Sender tab URL:", currentUrl);
+    console.log("request", request);
+    console.log("Sender tab URL:", currentUrl);
 
-  switch (request?.type) {
-    case "isLoggedIn": {
-      const isLoggedIn = await LocalDb.getLoggedIn();
+    switch (request?.type) {
+        case "isLoggedIn": {
+            const isLoggedIn = await LocalDb.getLoggedIn();
 
-      const matchedConfig = config.find((item) =>
-        currentUrl.includes(item.jobPortal.toLowerCase())
-      );
+            const matchedConfig = config.find((item) =>
+                currentUrl.includes(item.jobPortal.toLowerCase())
+            );
 
-      console.log("isLoggedIn", isLoggedIn, "matchedConfig", matchedConfig);
+            console.log(
+                "isLoggedIn",
+                isLoggedIn,
+                "matchedConfig",
+                matchedConfig
+            );
 
-      if (tabId) {
-        MessageBridge.sendToContentScript(
-          tabId,
-          {
-            type: "initPopup",
-            data: {
-              isLoggedIn: isLoggedIn ?? false,
-              config: matchedConfig,
-            },
-          },
-          false
-        );
-      } else {
-        console.warn("No tabId found for sender.");
-      }
+            if (tabId) {
+                MessageBridge.sendToContentScript(
+                    tabId,
+                    {
+                        type: "initPopup",
+                        data: {
+                            isLoggedIn: isLoggedIn ?? false,
+                            config: matchedConfig,
+                        },
+                    },
+                    false
+                );
+            } else {
+                console.warn("No tabId found for sender.");
+            }
 
-      return;
+            return;
+        }
+
+        case "getTabData": {
+            return tabData;
+        }
+
+        case "loginUser": {
+            const { email, password } = request.data || {};
+
+            if (email === "test@gmail.com" && password === "1234") {
+                await LocalDb.setLoggedIn(true);
+
+                return {
+                    status: true,
+                    message: "Login successful.",
+                };
+            }
+
+            return {
+                status: false,
+                message: "Invalid email or password.",
+            };
+        }
+
+        case "storeWhiteLabelValue": {
+            const { value } = request.data || {};
+            if (value) await LocalDb.insertToWhitelistKey(value);
+            return;
+        }
+
+        case "storeBlackLabelValue": {
+            const { value } = request.data || {};
+            if (value) await LocalDb.insertToBlacklistKey(value);
+            return;
+        }
+
+        case "getWhiteLabelValues": {
+            const values = await LocalDb.getWhitelistKeyValues();
+            return values;
+        }
+
+        case "getBlackLabelValues": {
+            const values = await LocalDb.getBlacklistKeyValues();
+            return values;
+        }
+
+        case "removeWhiteLabelValue": {
+            const { value } = request.data || {};
+            if (value) await LocalDb.removeFromList("whitelistKey", value);
+            return;
+        }
+
+        case "removeBlackLabelValue": {
+            const { value } = request.data || {};
+            if (value) await LocalDb.removeFromList("blacklistKey", value);
+            return;
+        }
+
+        case "sendDataToGoogleSheet": {
+            const { data } = request;
+            fetch(
+                "https://script.google.com/macros/s/AKfycbxFS0wn3gvPi7_Vqop6NvRP-gVpZmVEGbLVH5Kx2qDyUPgMHomO-DLr_DjZkOHQbzEZXg/exec",
+                {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((response) => response.json())
+                .then((res) => console.log("Google Sheet Response:", res))
+                .catch((err) =>
+                    console.log("Error pushing to Google Sheet", err.message)
+                );
+        }
+        default:
+            return;
     }
-
-    case "getTabData": {
-      return tabData;
-    }
-
-    case "loginUser": {
-      const { email, password } = request.data || {};
-
-      if (email === "test@gmail.com" && password === "1234") {
-        await LocalDb.setLoggedIn(true);
-
-        return {
-          status: true,
-          message: "Login successful.",
-        };
-      }
-
-      return {
-        status: false,
-        message: "Invalid email or password.",
-      };
-    }
-
-    case "storeWhiteLabelValue": {
-      const { value } = request.data || {};
-      if (value) await LocalDb.insertToWhitelistKey(value);
-      return;
-    }
-
-    case "storeBlackLabelValue": {
-      const { value } = request.data || {};
-      if (value) await LocalDb.insertToBlacklistKey(value);
-      return;
-    }
-
-    case "getWhiteLabelValues": {
-      const values = await LocalDb.getWhitelistKeyValues();
-      return values;
-    }
-
-    case "getBlackLabelValues": {
-      const values = await LocalDb.getBlacklistKeyValues();
-      return values;
-    }
-
-    case "removeWhiteLabelValue": {
-      const { value } = request.data || {};
-      if (value) await LocalDb.removeFromList("whitelistKey", value);
-      return;
-    }
-
-    case "removeBlackLabelValue": {
-      const { value } = request.data || {};
-      if (value) await LocalDb.removeFromList("blacklistKey", value);
-      return;
-    }
-
-    default:
-      return;
-  }
 });
--- common/js/content/copy-tab-handler.ts
+++ common/js/content/copy-tab-handler.ts
@@ -3,22 +3,22 @@ import $ from "jquery";
 import { MessageBridge } from "./messageBridge";
 import { highlightAndCountKeywords } from "./highlight/highlight";
 export function handleCopyJob($: Function, shadowRoot: ShadowRoot): void {
-  const $copyButton = $("#copy-btn-panel");
+    const $copyButton = $("#copy-btn-panel");
 
-  CopyTabPanel.copyJobDataToClipboard();
+    CopyTabPanel.copyJobDataToClipboard();
 }
 
 export async function handleAnalyseJob(
-  $: Function,
-  shadowRoot: ShadowRoot
+    $: Function,
+    shadowRoot: ShadowRoot
 ): Promise<void> {
-  CopyTabPanel.highlightAnalysisJobDescription(shadowRoot);
+    CopyTabPanel.highlightAnalysisJobDescription(shadowRoot);
 }
 
 export class CopyTabPanel {
-  // Static method to render the Copy Tab HTML
-  static renderCopyTab(): string {
-    return `
+    // Static method to render the Copy Tab HTML
+    static renderCopyTab(): string {
+        return `
       <div class="tab-pane fade show active" id="copy-tab-pane" role="tabpanel" aria-labelledby="copy-tab" tabindex="0">
       
      <div id="copy-btn-panel" class="d-flex flex-column justify-content-center align-items-center gap-3 min-vh-25 w-100">
@@ -47,89 +47,96 @@ export class CopyTabPanel {
       </div>
 
     `;
-  }
-
-  static async copyJobDataToClipboard(): Promise<void> {
-    const configStore = ConfigStore.getInstance();
-    const config = configStore.getConfig();
-
-    if (!config) {
-      console.warn("No configuration found for the current job portal.");
-      return;
     }
 
-    function getText(selectors: string | string[]): string {
-      const selArr = Array.isArray(selectors) ? selectors : [selectors];
+    static async copyJobDataToClipboard(): Promise<void> {
+        const configStore = ConfigStore.getInstance();
+        const config = configStore.getConfig();
 
-      for (const sel of selArr) {
-        const ele = $(sel);
-        if (ele.length > 0) {
-          const text = ele[0].textContent?.trim();
-          if (text) return text;
+        if (!config) {
+            console.warn("No configuration found for the current job portal.");
+            return;
         }
-      }
 
-      return "N/A";
+        function getText(selectors: string | string[]): string {
+            const selArr = Array.isArray(selectors) ? selectors : [selectors];
+
+            for (const sel of selArr) {
+                const ele = $(sel);
+                if (ele.length > 0) {
+                    const text = ele[0].textContent?.trim();
+                    if (text) return text;
+                }
+            }
+
+            return "N/A";
+        }
+
+        const companyName = getText(config.selectors.companyName.selector);
+        const location = getText(config.selectors.location.selector);
+        const title = getText(config.selectors.jobTitle.selector);
+        const url = window.location.href;
+
+        const row = `${companyName}\t${title}\t${location}\t${url}`;
+
+        try {
+            await navigator.clipboard.writeText(row);
+            //will be used later in the project
+            // sendDataToGoogleSheet({
+            //     company: companyName,
+            //     position: title,
+            //     location: location,
+            //     url: url
+            // });
+            console.log("Job data copied to clipboard (tabular format).");
+        } catch (err) {
+            console.error("Failed to copy job data to clipboard:", err);
+        }
     }
 
-    const companyName = getText(config.selectors.companyName.selector);
-    const location = getText(config.selectors.location.selector);
-    const title = getText(config.selectors.jobTitle.selector);
-    const url = window.location.href;
+    static async highlightAnalysisJobDescription(
+        shadowRoot: ShadowRoot
+    ): Promise<void> {
+        const configStore = ConfigStore.getInstance();
+        const config = configStore.getConfig();
 
-    const row = `${companyName}\t${title}\t${location}\t${url}`;
+        const selector = config?.selectors?.description?.selector;
+        if (!selector) {
+            console.warn("Job description selector not found in config.");
+            return;
+        }
 
-    try {
-      await navigator.clipboard.writeText(row);
-      console.log("Job data copied to clipboard (tabular format).");
-    } catch (err) {
-      console.error("Failed to copy job data to clipboard:", err);
-    }
-  }
+        try {
+            const [whitelist, blacklist] = await Promise.all([
+                MessageBridge.sendToServiceWorker(
+                    { type: "getWhiteLabelValues" },
+                    true
+                ),
+                MessageBridge.sendToServiceWorker(
+                    { type: "getBlackLabelValues" },
+                    true
+                ),
+            ]);
 
-  static async highlightAnalysisJobDescription(
-    shadowRoot: ShadowRoot
-  ): Promise<void> {
-    const configStore = ConfigStore.getInstance();
-    const config = configStore.getConfig();
+            const result = highlightAndCountKeywords(
+                selector,
+                whitelist || [],
+                blacklist || []
+            );
 
-    const selector = config?.selectors?.description?.selector;
-    if (!selector) {
-      console.warn("Job description selector not found in config.");
-      return;
-    }
+            console.log("Highlighting complete. Match counts:", result);
 
-    try {
-      const [whitelist, blacklist] = await Promise.all([
-        MessageBridge.sendToServiceWorker(
-          { type: "getWhiteLabelValues" },
-          true
-        ),
-        MessageBridge.sendToServiceWorker(
-          { type: "getBlackLabelValues" },
-          true
-        ),
-      ]);
+            const displayList = shadowRoot.querySelector("#display-list");
 
-      const result = highlightAndCountKeywords(
-        selector,
-        whitelist || [],
-        blacklist || []
-      );
-
-      console.log("Highlighting complete. Match counts:", result);
-
-      const displayList = shadowRoot.querySelector("#display-list");
-
-      if (displayList) {
-        displayList.innerHTML = `
+            if (displayList) {
+                displayList.innerHTML = `
     <div class="alert alert-light border p-3 my-4">
       <div class="d-flex justify-content-around mb-3">
         <span class="badge bg-success fs-6 px-3 py-2">Whitelist: ${
-          result.whitelistCount
+            result.whitelistCount
         }</span>
         <span class="badge bg-danger fs-6 px-3 py-2">Blacklist: ${
-          result.blacklistCount
+            result.blacklistCount
         }</span>
       </div>
       <hr>
@@ -138,14 +145,14 @@ export class CopyTabPanel {
           <h6 class='fw-bold'>Matched Whitelist</h6>
           <div class="d-flex flex-wrap gap-2  badge-container">
            ${
-             result.matchedWhitelist.length
-               ? result.matchedWhitelist
-                   .map(
-                     (w) =>
-                       `<span class="badge bg-success  p-2 rounded-pill">${w}</span>`
-                   )
-                   .join("")
-               : "None"
+               result.matchedWhitelist.length
+                   ? result.matchedWhitelist
+                         .map(
+                             (w) =>
+                                 `<span class="badge bg-success  p-2 rounded-pill">${w}</span>`
+                         )
+                         .join("")
+                   : "None"
            }
           </div>
         </div>
@@ -154,28 +161,35 @@ export class CopyTabPanel {
           <h6 class='fw-bold'>Matched Blacklist</h6>
           <div class="d-flex flex-wrap gap-2  badge-container">
           ${
-            result.matchedBlacklist.length
-              ? result.matchedBlacklist
-                  .map(
-                    (w) =>
-                      `<span class="badge bg-danger  p-2 rounded-pill">${w}</span>`
-                  )
-                  .join("")
-              : "None"
+              result.matchedBlacklist.length
+                  ? result.matchedBlacklist
+                        .map(
+                            (w) =>
+                                `<span class="badge bg-danger  p-2 rounded-pill">${w}</span>`
+                        )
+                        .join("")
+                  : "None"
           }
           </div>
         </div>
       </div>
     </div>
   `;
-      } else {
-        console.warn("#display-list not found inside Shadow DOM.");
-      }
-    } catch (err) {
-      console.error(
-        "Failed to fetch keywords or highlight job description:",
-        err
-      );
+            } else {
+                console.warn("#display-list not found inside Shadow DOM.");
+            }
+        } catch (err) {
+            console.error(
+                "Failed to fetch keywords or highlight job description:",
+                err
+            );
+        }
     }
-  }
+}
+
+function sendDataToGoogleSheet(data) {
+  MessageBridge.sendToServiceWorker(
+    { type: "sendDataToGoogleSheet", data },
+    false
+  );
 }
